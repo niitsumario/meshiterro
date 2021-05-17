f8361f36b6691971f7b350062edb3a01920e5e1c
niitsumario <xxx.ballet121.xxx@gmail.com>
2021-05-17 15:39:37 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '21389fe6007920625406f446e2068a45c4b4cd50cc204ad0123260d3ee2d05966631626c59c38ebba4c5e2c56d2b03529efc80e83aca00bffa4ce38155318783'
